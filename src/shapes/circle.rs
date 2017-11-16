ccda3708abddafe11ea913bebd743026d6cc78d8
Arav Singhal <singhal.arav@gmail.com>
2017-11-16 20:47:04 +0530

    Complete Matter impl for Polygon and Circle

--- src/shapes/circle.rs
+++ src/shapes/circle.rs
@@ -1,9 +1,14 @@
+use math::*;
+
 pub struct Circle {
     pub radius: f32,
 }
 
 impl super::Matter for Circle {
     fn mass_and_inertia(&self, density: f32) -> (f32, f32) {
-        unimplemented!()
+        let mass = density * PI * self.radius * self.radius;
+        let inertia = 0.5 * mass * self.radius * self.radius;
+        
+        (mass, inertia)
     }
 }
--- src/shapes/polygon.rs
+++ src/shapes/polygon.rs
@@ -5,8 +5,33 @@ pub struct Polygon {
     pub normals: Vec<Vec2>,
 }
 
+impl Polygon {
+    
+    #[inline]
+    pub fn vert_count(&self) -> usize {
+        self.vertices.len()
+    }
+}
+
+
 impl super::Matter for Polygon {
     fn mass_and_inertia(&self, density: f32) -> (f32, f32) {
-        unimplemented!()
+        let mut area = 0f32;
+        let mut density_inertia = 0f32;
+        
+        for i in 0..self.vert_count() {
+            let j: usize = (i + 1) % self.vert_count();
+            
+            let p1 = self.vertices[i];
+            let p2 = self.vertices[j];
+            
+            let tri_area = 0.5 * p1.cross(p2).abs();
+            let tri_inertia = tri_area * (p1.sqr_len() + p2.sqr_len() + p1.dot(p2)) / 6.0;
+            
+            area += tri_area;
+            density_inertia += tri_inertia;
+        }
+    
+        (area * density, density_inertia * density)
     }
 }
