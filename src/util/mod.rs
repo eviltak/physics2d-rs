bd943eb8dda2ea26d624ec564d08b98f16ab1952
Arav Singhal <singhal.arav@gmail.com>
2018-11-07 20:24:29 -0600

    Add matcher macros for all possible method receivers

--- src/util/mod.rs
+++ src/util/mod.rs
@@ -1,6 +1,16 @@
 pub mod pool;
 
-macro_rules! match_fn_to_variants {
+macro_rules! match_owned_fn_to_variants {
+    ($enum_name:ident::{$($variant:ident),*}; $val:expr, $fn_name:ident$args:tt) => {
+        match $val {
+            $(
+            $enum_name::$variant(_data) =>  _data.$fn_name$args,
+            )*
+        }
+    };
+}
+
+macro_rules! match_borrow_fn_to_variants {
     ($enum_name:ident::{$($variant:ident),*}; $val:expr, $fn_name:ident$args:tt) => {
         match $val {
             $(
@@ -10,11 +20,41 @@ macro_rules! match_fn_to_variants {
     };
 }
 
-macro_rules! generate_match_fn_macro_for_enum {
+macro_rules! match_mut_fn_to_variants {
+    ($enum_name:ident::{$($variant:ident),*}; $val:expr, $fn_name:ident$args:tt) => {
+        match $val {
+            $(
+            $enum_name::$variant(ref mut _data) =>  _data.$fn_name$args,
+            )*
+        }
+    };
+}
+
+macro_rules! generate_match_owned_fn_macro_for_enum {
     ($enum_name:ident::{$($variant:ident),*}; $macro_name:ident) => {
         macro_rules! $macro_name {
             ($val:expr, $fn_name:ident$args:tt) => {
-                match_fn_to_variants!($enum_name::{$($variant),*}; $val, $fn_name$args)
+                match_owned_fn_to_variants!($enum_name::{$($variant),*}; $val, $fn_name$args)
+            };
+        }
+    };
+}
+
+macro_rules! generate_match_borrow_fn_macro_for_enum {
+    ($enum_name:ident::{$($variant:ident),*}; $macro_name:ident) => {
+        macro_rules! $macro_name {
+            ($val:expr, $fn_name:ident$args:tt) => {
+                match_borrow_fn_to_variants!($enum_name::{$($variant),*}; $val, $fn_name$args)
+            };
+        }
+    };
+}
+
+macro_rules! generate_match_mut_fn_macro_for_enum {
+    ($enum_name:ident::{$($variant:ident),*}; $macro_name:ident) => {
+        macro_rules! $macro_name {
+            ($val:expr, $fn_name:ident$args:tt) => {
+                match_mut_fn_to_variants!($enum_name::{$($variant),*}; $val, $fn_name$args)
             };
         }
     };
